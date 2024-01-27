e11484a83fdf49df4d1dfef7f45f077fcabda996
Helena Żakowicz <hela.zakow@gmail.com>
2024-01-27 14:00:34 +0100

    initial commit

---
+++ python.py
@@ -0,0 +1,9 @@
+
+def potęga(x,y):
+    wyn=1
+    for i in range(y):
+        wyn*=x
+
+    return wyn
+
+
---
+++ test_python.py
@@ -0,0 +1,7 @@
+from python import *
+
+
+def test_potęga():
+    a = 2
+    b = 4
+    assert potęga(a,b) == pow(a,b)
